4cef551ad32527f0d5ecaa902c4466476fefc129
Sören Kuklau <kuklau@trenz.de>
2021-05-08 00:03:18 +0200

    early compiling template

---
+++ AltNetworkUtility.macOS/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using AppKit;
+
+using Foundation;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace AltNetworkUtility.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public override NSWindow MainWindow { get; }
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "Xamarin.Forms on Mac!", // choose your own Title here
+                TitleVisibility = NSWindowTitleVisibility.Hidden
+            };
+        }
+
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+
+            LoadApplication(new App());
+
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification) { }
+    }
+}
