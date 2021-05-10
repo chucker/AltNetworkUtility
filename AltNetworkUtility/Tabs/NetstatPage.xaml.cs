3883c390d5b3404b6d506f110f7f5afb5017782b
Sören Kuklau <chucker23n+github@gmail.com>
2021-05-11 01:19:01 +0200

    Early stuff for #9 Netstat tab

---
+++ AltNetworkUtility/Tabs/NetstatPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace AltNetworkUtility.Tabs
+{
+    public partial class NetstatPage : ContentPage
+    {
+        public NetstatPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ContentPage_BindingContextChanged(object sender, System.EventArgs e)
+        {
+            if (BindingContext is NetstatPageViewModel viewModel)
+                await viewModel.InitAsync();
+        }
+    }
+}
---
+++ AltNetworkUtility/Tabs/NetstatPageViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using CliWrap.Buffered;
+
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace AltNetworkUtility.Tabs
+{
+    public enum NetstatMode
+    {
+        _ = 0,
+        RoutingTable,
+        NetworkStatistics,
+        MulticastInformation,
+        SocketState
+    }
+
+    public class NetstatPageViewModel : ViewModelBase
+    {
+        private NetstatMode _Mode = NetstatMode.RoutingTable;
+        public NetstatMode Mode
+        {
+            get => _Mode;
+            set => SetProperty(ref _Mode, value);
+        }
+
+        private bool _What;
+        public bool What
+        {
+            get => _What;
+            set => SetProperty(ref _What, value);
+        }
+
+        private bool _The;
+        public bool The
+        {
+            get => _The;
+            set => SetProperty(ref _The, value);
+        }
+
+        private bool _Hell;
+        public bool Hell
+        {
+            get => _Hell;
+            set => SetProperty(ref _Hell, value);
+        }
+
+        private string _Output = "";
+        public string Output
+        {
+            get => _Output;
+            set => SetProperty(ref _Output, value);
+        }
+
+        public IAsyncRelayCommand NetstatCommand { get; }
+
+        public NetstatPageViewModel()
+        {
+            NetstatCommand = new AsyncRelayCommand(
+                Netstat,
+                () => Mode > 0);
+        }
+
+        public async Task InitAsync()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public async Task Netstat()
+        {
+            var result = await CliWrap.Cli.Wrap("/usr/sbin/netstat")
+                                      .WithArguments("-nr")
+                                      .ExecuteBufferedAsync();
+            Output = result.StandardOutput;
+        }
+    }
+}
