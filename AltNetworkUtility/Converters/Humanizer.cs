ee71c9484b5f2d127e29e3461e7787c9253f6626
Sören Kuklau <chucker23n+github@gmail.com>
2021-05-18 00:10:38 +0200

    Trying a pluralize converter.
    Unfortunately, the XAML here is quite verbose, and:
    
    * we can't use `Source='IP Addresses'` at compile time because the XAML compiler will reject it — even though this works at runtime (XML entities might work?)
    * the converter parameter points to the binding, not the value. Therefore, this doesn't actually work.

---
+++ AltNetworkUtility/Converters/Humanizer.cs
@@ -0,0 +1,22 @@
+﻿using Humanizer;
+
+using LambdaConverters;
+
+using System.Collections;
+
+using Xamarin.Forms;
+
+namespace AltNetworkUtility.Converters
+{
+    public static class Humanizer
+    {
+        public static readonly IValueConverter Pluralize =
+            ValueConverter.Create<string, string, ICollection?>(vca =>
+            {
+                if (vca.Parameter?.Count != 1)
+                    return vca.Value.Pluralize();
+
+                return vca.Value;
+            });
+    }
+}
