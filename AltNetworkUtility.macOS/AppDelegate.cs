a7b028ba2e84547ee3156f5b209ab890debdbfbe
Sören Kuklau <chucker23n+github@gmail.com>
2021-05-23 00:02:29 +0200

    Implements #21 audible ping

--- AltNetworkUtility.macOS/AppDelegate.cs
+++ AltNetworkUtility.macOS/AppDelegate.cs
@@ -21,6 +21,7 @@ namespace AltNetworkUtility.macOS
         {
             DependencyService.Register<IIconFontProvider, MacIconFontProvider>();
             DependencyService.Register<INetworkInterfacesService, MacNetworkInterfacesService>();
+            DependencyService.Register<ISystemSoundService, MacSystemSoundService>();
 
             var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
             var rect = new CoreGraphics.CGRect(200, 200, 640, 388);
