a5740e6721a58546aa0db2851d069e9e1d925fce
Sören Kuklau <chucker23n+github@gmail.com>
2021-05-24 22:59:42 +0200

    custom-listview-renderer: (Mostly) 1:1 fork

---
+++ AltNetworkUtility.macOS/Renderers/Internals/NSTableViewCellStyle.cs
@@ -0,0 +1,13 @@
+﻿namespace AltNetworkUtility.macOS.Renderers.Internals
+{
+    internal enum NSTableViewCellStyle
+	{
+		Default,
+		Value1,
+		Value2,
+		Subtitle,
+		Image,
+		ImageSubtitle,
+		Empty
+	}
+}
