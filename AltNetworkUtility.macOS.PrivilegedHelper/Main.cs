f5f2f8a49b6c595f11dd56ece8652fbdca4eb548
Sören Kuklau <chucker23n+github@gmail.com>
2021-06-01 20:55:52 +0200

    prototypes/SMJobBless: stub for a privileged helper tool

---
+++ AltNetworkUtility.macOS.PrivilegedHelper/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace AltNetworkUtility.macOS.PrivilegedHelper
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
